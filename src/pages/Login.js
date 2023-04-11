b15be3d77e0ad16f3501b152d02902516b67882f
Gabriel Galeski <galeski35@gmail.com>
2023-04-11 15:50:56 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,74 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { userInfos } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    this.setState({
+      [name]: target.value,
+    });
+  };
+
+  changePage = () => {
+    const { history, dispatch } = this.props;
+    const { email } = this.state;
+    dispatch(userInfos(email));
+    return history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const minPassword = 6;
+    return (
+      <div>
+        <label htmlFor="email">
+          <input
+            placeholder="Email"
+            id="email"
+            name="email"
+            data-testid="email-input"
+            type="email"
+            onChange={ this.handleChange }
+            value={ email }
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            placeholder="Password"
+            id="password"
+            name="password"
+            data-testid="password-input"
+            type="password"
+            onChange={ this.handleChange }
+            value={ password }
+          />
+        </label>
+        <button
+          disabled={
+            !(password.length >= minPassword
+              && email.includes('@') && email.includes('.com'))
+          }
+          onClick={ this.changePage }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+export const GET_USER_INFOS = 'GET_USER_INFOS';
+
+export const userInfos = (email) => ({
+  type: GET_USER_INFOS,
+  payload: {
+    email,
+  },
+});
