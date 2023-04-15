09e6d1c365e5afb413a3bd798cf7ee82da2b0efb
Gabriel Galeski <galeski35@gmail.com>
2023-04-15 13:46:02 -0300

    Iniciando estilização

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { userInfos } from '../redux/actions';
+import logo from '../images/logo.svg';
 
 class Login extends React.Component {
   state = {
@@ -27,41 +28,50 @@ class Login extends React.Component {
     const { email, password } = this.state;
     const minPassword = 6;
     return (
-      <form>
-        <label htmlFor="email">
-          Email
-          <input
-            placeholder="Email"
-            id="email"
-            name="email"
-            data-testid="email-input"
-            type="email"
-            onChange={ this.handleChange }
-            value={ email }
-          />
-        </label>
-        <label htmlFor="password">
-          Senha
-          <input
-            placeholder="Password"
-            id="password"
-            name="password"
-            data-testid="password-input"
-            type="password"
-            onChange={ this.handleChange }
-            value={ password }
-          />
-        </label>
-        <button
-          type="button"
-          disabled={
-            !(password.length >= minPassword
+      <form className="form-login">
+        <div className="inputs-container">
+          <section>
+            <img src={ logo } alt="logo" />
+            <h1>
+              Trybe
+              <span>Wallet</span>
+            </h1>
+          </section>
+          <label htmlFor="email">
+            Email
+            <input
+              placeholder="Email"
+              id="email"
+              name="email"
+              data-testid="email-input"
+              type="email"
+              onChange={ this.handleChange }
+              value={ email }
+            />
+          </label>
+          <label htmlFor="password">
+            Senha
+            <input
+              placeholder="Password"
+              id="password"
+              name="password"
+              data-testid="password-input"
+              type="password"
+              onChange={ this.handleChange }
+              value={ password }
+            />
+          </label>
+          <button
+            type="button"
+            disabled={
+              !(password.length >= minPassword
               && email.includes('@') && email.includes('.com'))
-          }
-          onClick={ this.changePage }
-        >
-          Entrar
-        </button>
+            }
+            onClick={ this.changePage }
+          >
+            Entrar
+          </button>
+        </div>
       </form>
     );
   }
